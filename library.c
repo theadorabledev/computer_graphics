d2d2b661d7fd524c727669cd63df759c88541457
Theadora Williams <theadorabledeveloper@gmail.com>
2020-04-23 19:16:19 -0400

    Added debugging to makefile and added a string replacement function.

--- library.c
+++ library.c
@@ -908,3 +908,42 @@ void torus(ELEMENT * e, double x, double y, double z, double r, double R){
   }
   free_matrix(m);
 }
+
+char *str_replace(char *orig, char *rep, char *with) {
+  //https://stackoverflow.com/questions/779875/what-is-the-function-to-replace-string-in-c
+  char *result; // the return string
+  char *ins;    // the next insert point
+  char *tmp;    // varies
+  int len_rep;  // length of rep (the string to remove)
+  int len_with; // length of with (the string to replace rep with)
+  int len_front; // distance between rep and end of last rep
+  int count;    // number of replacements
+
+  if (!orig || !rep)
+    return NULL;
+  len_rep = strlen(rep);
+  if (len_rep == 0)
+    return NULL; // empty rep causes infinite loop during count
+  if (!with)
+    with = "";
+  len_with = strlen(with);
+
+  ins = orig;
+  for (count = 0; tmp = strstr(ins, rep); ++count) {
+    ins = tmp + len_rep;
+  }
+  tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+  if (!result)
+    return NULL;
+  while (count--) {
+    ins = strstr(orig, rep);
+    len_front = ins - orig;
+    tmp = strncpy(tmp, orig, len_front) + len_front;
+    tmp = strcpy(tmp, with) + len_with;
+    orig += len_front + len_rep; // move to next "end of rep"
+  }
+  strcpy(tmp, orig);
+  return result;
+}
+
